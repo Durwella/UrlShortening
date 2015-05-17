915b5e70abbb682e5eec63769fce0574c83eb3a3
Jacob Foshee <jacobf@gmail.com>
2015-05-17 17:24:28 -0500

    Avoid creating short url which redirects to a redirect (e.g. a loop, or series of redirects)

--- Durwella.UrlShortening.Tests/UrlShortenerTest.cs
+++ Durwella.UrlShortening.Tests/UrlShortenerTest.cs
@@ -6,68 +6,85 @@ namespace Durwella.UrlShortening.Tests
 {
     public class UrlShortenerTest
     {
+        UrlShortener _subject;
+        private Mock<IUrlUnwrapper> _mockUnwrapper;
+        private Mock<IHashScheme> _mockHashScheme;
+        private Mock<IAliasRepository> _mockRepository;
+
+        [SetUp]
+        public void SetupSubject()
+        {
+            var baseUrl = "http://goto";
+            _mockHashScheme = new Mock<IHashScheme>();
+            _mockRepository = new Mock<IAliasRepository>();
+            _mockUnwrapper = new Mock<IUrlUnwrapper>();
+            _mockUnwrapper.Setup(u => u.GetDirectUrl(It.IsAny<string>())).Returns<string>(s => s);
+            _subject = new UrlShortener(baseUrl, _mockRepository.Object, _mockHashScheme.Object, _mockUnwrapper.Object);
+        }
+
         [Test]
         public void ShouldSaveAndReturnHash()
         {
-            var mockHashScheme = new Mock<IHashScheme>();
             var url = "http://example.com/foo/bar";
             var hash = "123abc";
-            mockHashScheme.Setup(h => h.GetKey(url)).Returns(hash);
-            var mockRepository = new Mock<IAliasRepository>();
-            var baseUrl = "http://goto";
-            var subject = new UrlShortener(mockHashScheme.Object, mockRepository.Object, baseUrl);
+            _mockHashScheme.Setup(h => h.GetKey(url)).Returns(hash);
 
-            var shortened = subject.Shorten(url);
+            var shortened = _subject.Shorten(url);
 
             shortened.Should().Be("http://goto/123abc");
-            mockRepository.Verify(r => r.Add(hash, url));
+            _mockRepository.Verify(r => r.Add(hash, url));
         }
 
         [Test]
         public void ShouldReturnExistingHash()
         {
-            var mockHashScheme = new Mock<IHashScheme>();
             var url = "http://example.com/a/b/c";
             var hash = "abc";
-            var mockRepository = new Mock<IAliasRepository>();
-            mockRepository.Setup(r => r.ContainsValue(url)).Returns(true);
-            mockRepository.Setup(r => r.GetKey(url)).Returns(hash);
-            var baseUrl = "http://go2";
-            var subject = new UrlShortener(mockHashScheme.Object, mockRepository.Object, baseUrl);
+            _mockRepository.Setup(r => r.ContainsValue(url)).Returns(true);
+            _mockRepository.Setup(r => r.GetKey(url)).Returns(hash);
 
-            var shortened = subject.Shorten(url);
+            var shortened = _subject.Shorten(url);
 
-            shortened.Should().Be("http://go2/abc");
-            mockRepository.Verify(r => r.Add(hash, url), Times.Never());
-            mockHashScheme.Verify(h => h.GetKey(url), Times.Never());
+            shortened.Should().Be("http://goto/abc");
+            _mockRepository.Verify(r => r.Add(hash, url), Times.Never());
+            _mockHashScheme.Verify(h => h.GetKey(url), Times.Never());
         }
 
         [Test]
         public void HandleHashCollision()
         {
-            var mockHashScheme = new Mock<IHashScheme>();
             var url = "http://example.com/hash/collision";
             var hash1 = "aaaa";
             var hash2 = "bbbb";
             var hash3 = "cccc";
-            mockHashScheme.Setup(h => h.GetKey(url)).Returns(hash1);
-            mockHashScheme.Setup(h => h.GetKey(url, 0)).Returns(hash1);
-            mockHashScheme.Setup(h => h.GetKey(url, 1)).Returns(hash2);
-            mockHashScheme.Setup(h => h.GetKey(url, 2)).Returns(hash3);
-            var mockRepository = new Mock<IAliasRepository>();
-            mockRepository.Setup(r => r.ContainsValue(url)).Returns(false);
-            mockRepository.Setup(r => r.ContainsKey(hash1)).Returns(true);
-            mockRepository.Setup(r => r.ContainsKey(hash2)).Returns(true);
-            mockRepository.Setup(r => r.ContainsKey(hash3)).Returns(false);
-            var baseUrl = "http://go";
-            var subject = new UrlShortener(mockHashScheme.Object, mockRepository.Object, baseUrl);
+            _mockHashScheme.Setup(h => h.GetKey(url)).Returns(hash1);
+            _mockHashScheme.Setup(h => h.GetKey(url, 0)).Returns(hash1);
+            _mockHashScheme.Setup(h => h.GetKey(url, 1)).Returns(hash2);
+            _mockHashScheme.Setup(h => h.GetKey(url, 2)).Returns(hash3);
+            _mockRepository.Setup(r => r.ContainsValue(url)).Returns(false);
+            _mockRepository.Setup(r => r.ContainsKey(hash1)).Returns(true);
+            _mockRepository.Setup(r => r.ContainsKey(hash2)).Returns(true);
+            _mockRepository.Setup(r => r.ContainsKey(hash3)).Returns(false);
 
-            var shortened = subject.Shorten(url);
+            var shortened = _subject.Shorten(url);
 
-            shortened.Should().Be("http://go/cccc");
-            mockRepository.Verify(r => r.Add(hash3, url));
+            shortened.Should().Be("http://goto/cccc");
+            _mockRepository.Verify(r => r.Add(hash3, url));
         }
 
-        // TODO: Redirection unwrapping (avoid multiple redirects and redirect loops)
+        [Test]
+        public void ShouldUnwrapUrlBeforeShortening()
+        {
+            var givenUrl = "http://t.co/123";
+            var url = "http://example.com/abc";
+            var hash = "foo";
+            _mockUnwrapper.Setup(u => u.GetDirectUrl(givenUrl)).Returns(url);
+            _mockHashScheme.Setup(h => h.GetKey(url)).Returns(hash);
+
+            var shortened = _subject.Shorten(givenUrl);
+
+            shortened.Should().Be("http://goto/foo");
+            _mockRepository.Verify(r => r.Add(hash, url));
+        }
     }
 }
--- Durwella.UrlShortening/UrlShortener.cs
+++ Durwella.UrlShortening/UrlShortener.cs
@@ -6,22 +6,35 @@ namespace Durwella.UrlShortening
     {
         public IHashScheme HashScheme { get; private set; }
         public IAliasRepository Repository { get; private set; }
+        public IUrlUnwrapper UrlUnwrapper { get; private set; }
         public string BaseUrl { get; private set; }
         public const int MaximumHashAttempts = 500;
 
         public UrlShortener()
-            : this( new DefaultHashScheme(), new MemoryAliasRepository(), "http://example.com")
+            : this("http://example.com", new MemoryAliasRepository(), new DefaultHashScheme(), new WebClientUrlUnwrapper())
         {
         }
 
-        public UrlShortener(IHashScheme hashScheme, IAliasRepository repository, string baseUrl)
+        public UrlShortener(string baseUrl, IAliasRepository repository, IHashScheme hashScheme)
+            : this(baseUrl, repository, hashScheme, new WebClientUrlUnwrapper())
         {
-            HashScheme = hashScheme;
-            Repository = repository;
+        }
+
+        public UrlShortener(string baseUrl, IAliasRepository repository, IHashScheme hashScheme, IUrlUnwrapper urlUnwrapper)
+        {
             BaseUrl = baseUrl;
+            Repository = repository;
+            HashScheme = hashScheme;
+            UrlUnwrapper = urlUnwrapper;
         }
 
         public string Shorten(string url)
+        {
+            var directUrl = UrlUnwrapper.GetDirectUrl(url);
+            return ShortenDirect(directUrl);
+        }
+
+        private string ShortenDirect(string url)
         {
             string key;
             if (Repository.ContainsValue(url))
