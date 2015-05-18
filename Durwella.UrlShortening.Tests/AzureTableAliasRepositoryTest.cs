2406e5671f518a1a1c390d34f2b0547c0fee9ee5
Jacob Foshee <jacobf@gmail.com>
2015-05-18 12:28:43 -0500

    Started Azure Table IAliasRepository.  These will be 'live' tests so they need some special treatment.

---
+++ Durwella.UrlShortening.Tests/AzureTableAliasRepositoryTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
+using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace Durwella.UrlShortening.Tests
+{
+    [Explicit]
+    public class AzureTableAliasRepositoryTest
+    {
+        private static string _azureStorageAccountName;
+        private static string _azureStorageAccessKey;
+        private static bool _enabled = false;
+
+        [TestFixtureSetUp]
+        public static void LoadAzureCredentials()
+        {
+            var credentialsFile = File.ReadAllLines("AzureTestCredentials.txt");
+            if (credentialsFile.Length < 2
+                || String.IsNullOrWhiteSpace(credentialsFile[0])
+                || String.IsNullOrWhiteSpace(credentialsFile[1]))
+                return;
+            _azureStorageAccountName = credentialsFile[0];
+            _azureStorageAccessKey = credentialsFile[1];
+            _enabled = true;
+        }
+
+        private static CloudTableClient MakeTableClient()
+        {
+            var credentials = new StorageCredentials(_azureStorageAccountName, _azureStorageAccessKey);
+            var account = new CloudStorageAccount(credentials, useHttps: true);
+            return account.CreateCloudTableClient();
+        }
+
+        [Test]
+        public void ShouldCreateTable()
+        {
+            if (!_enabled) Assert.Ignore("Populate AzureTestCredentials.txt for this test");
+            string tablePrefix = "UrlShortening";
+
+            var subject = new AzureTableAliasRepository(_azureStorageAccountName, _azureStorageAccessKey, tablePrefix);
+
+            var tableClient = MakeTableClient();
+            var table = tableClient.GetTableReference(tablePrefix);
+            Assert.IsTrue(table.Exists());
+        }
+    }
+}
---
+++ Durwella.UrlShortening/AzureTableAliasRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using System;
+
+namespace Durwella.UrlShortening
+{
+    public class AzureTableAliasRepository : IAliasRepository
+    {
+        public AzureTableAliasRepository(string azureStorageAccountName, string azureStorageAccessKey, string tablePrefix)
+        {
+            var credentials = new StorageCredentials(azureStorageAccountName, azureStorageAccessKey);
+            var account = new CloudStorageAccount(credentials, useHttps: true);
+            var tableClient = account.CreateCloudTableClient();
+            var table = tableClient.GetTableReference(tablePrefix);
+            table.CreateIfNotExists();
+        }
+
+        public void Add(string key, string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsKey(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsValue(string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetKey(string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetValue(string key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
