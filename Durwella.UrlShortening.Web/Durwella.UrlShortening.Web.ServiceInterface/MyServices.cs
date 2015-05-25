b14675ef07a89f4b56586eb8007004f194219ca2
Jacob Foshee <jacobf@gmail.com>
2015-05-25 14:01:47 -0500

    Started new projects for web site/service using AngularJS template from ServiceStackVS add-in.

---
+++ Durwella.UrlShortening.Web/Durwella.UrlShortening.Web.ServiceInterface/MyServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ServiceStack;
+using Durwella.UrlShortening.Web.ServiceModel;
+
+namespace Durwella.UrlShortening.Web.ServiceInterface
+{
+    public class MyServices : Service
+    {
+        public object Any(Hello request)
+        {
+            return new HelloResponse { Result = "Hello, {0}!".Fmt(request.Name) };
+        }
+    }
+}
---
+++ Durwella.UrlShortening.Web/Durwella.UrlShortening.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Durwella.UrlShortening.Web.Global" Language="C#" %>
