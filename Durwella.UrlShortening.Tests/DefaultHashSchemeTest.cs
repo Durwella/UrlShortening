e1a287c8cde9df5e24c86dd330a171c37c1be197
Jacob Foshee <jacobf@gmail.com>
2015-05-25 19:55:36 -0500

    Improved default hash, although it is still not good for this purpose.

---
+++ Durwella.UrlShortening.Tests/DefaultHashSchemeTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Durwella.UrlShortening.Tests
+{
+    public class DefaultHashSchemeTest
+    {
+        [Test]
+        public void ExamplesOfDefaultHash()
+        {
+            var scheme = new DefaultHashScheme();
+            Console.WriteLine(scheme.GetKey("foo"));
+            Console.WriteLine(scheme.GetKey("foo", 1));
+            Console.WriteLine(scheme.GetKey("goo"));
+            Console.WriteLine(scheme.GetKey("http://faslokdfjasf.asdfj.asdf.sa.fs.df.asdfa.vvdhh.aeh/efiab/esbaseieia/vea"));
+        }
+
+    }
+}
