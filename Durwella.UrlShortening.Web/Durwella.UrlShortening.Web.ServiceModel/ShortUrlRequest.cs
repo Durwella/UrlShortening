82b5bef6ad76816262c72281c2ad655eb38ef4ac
Jacob Foshee <jacobf@gmail.com>
2015-06-14 14:31:22 -0500

    Added an "API" for the swagger generation

--- Durwella.UrlShortening.Web/Durwella.UrlShortening.Web.ServiceModel/ShortUrlRequest.cs
+++ Durwella.UrlShortening.Web/Durwella.UrlShortening.Web.ServiceModel/ShortUrlRequest.cs
@@ -2,7 +2,8 @@
 
 namespace Durwella.UrlShortening.Web.ServiceModel
 {
-    [Route("/shorten", "POST", Summary = "Create a short URL that will redirect to the given URL.", Notes = "The given URL should be well-formatted and live/reachable.")]
+    [Api("URL Shortening")]
+    [Route("/shorten", "POST", Summary = "Create a short URL that will redirect to the given URL.", Notes = "The given URL should be well-formatted and live or reachable.")]
     public class ShortUrlRequest
     {
         public string Url { get; set; }
