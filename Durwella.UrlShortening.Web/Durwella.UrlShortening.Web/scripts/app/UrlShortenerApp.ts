66b1314557e74f388a20696cb502cc8e34b45b7e
Jacob Foshee <jacobf@gmail.com>
2015-08-08 17:16:01 -0500

    Using more strongly typed response callbacks. Also fixed the jquery include. And starting to carve place to respond to 401-unauthorized.

--- Durwella.UrlShortening.Web/Durwella.UrlShortening.Web/scripts/app/UrlShortenerApp.ts
+++ Durwella.UrlShortening.Web/Durwella.UrlShortening.Web/scripts/app/UrlShortenerApp.ts
@@ -6,6 +6,9 @@ module UrlShortenerApp {
     import ShortUrlResponse = Durwella.UrlShortening.Web.ServiceModel.ShortUrlResponse;
     import ShortUrlRequest = Durwella.UrlShortening.Web.ServiceModel.ShortUrlRequest;
     import ResponseStatus = Durwella.UrlShortening.Web.ServiceModel.ResponseStatus;
+    import HttpPromiseCallback = angular.IHttpPromiseCallback;
+    import HttpHeadersGetter = angular.IHttpHeadersGetter;
+    import RequestConfig = angular.IRequestConfig;
 
     interface IUrlShortenerScope extends ng.IScope {
         longUrl: string;
@@ -14,6 +17,20 @@ module UrlShortenerApp {
         errorMessage: string;
         waiting: boolean;
         submitLongUrl: Function;
+        unauthorized: boolean;
+    }
+
+    enum HttpStatusCodes {
+        BadRequest = 400,
+        Unauthorized = 401,
+        PaymentRequired = 402,
+        Forbidden = 403,
+        NotFound = 404,
+        MethodNotAllowed = 405,
+        NotAcceptable = 406,
+        ProxyAuthenticationRequired = 407,
+        RequestTimeout = 408
+        //...
     }
 
     class UrlShortenerCtrl {
@@ -41,23 +58,32 @@ module UrlShortenerApp {
                 $scope.shortenedUrl = null;
                 $scope.errorMessage = null;
                 $scope.waiting = true;
-                $http.post("/shorten", <ShortUrlRequest> { Url: $scope.longUrl, CustomPath: $scope.customPath })
-                    .success((response: ShortUrlResponse) => {
-                        $scope.shortenedUrl = response.Shortened;
-                        $scope.waiting = false;
-                    })
-                    .error((response: any) => {
-                        console.log("Error: ", response);
-                        var message = "Unknown Error";
-                        if (response.hasOwnProperty("ResponseStatus")) {
-                            var responseStatus = <ResponseStatus>response.ResponseStatus;
-                            message = responseStatus.Message;
-                        }
-                        $scope.errorMessage = message;
-                        $scope.waiting = false;
-                    });
+                var request = <ShortUrlRequest> { Url: $scope.longUrl, CustomPath: $scope.customPath };
+                $http.post("/shorten", request)
+                    .success(this.onSuccess)
+                    .error(this.onError);
             };
         }
+
+        onSuccess: HttpPromiseCallback<ShortUrlResponse> = (response: ShortUrlResponse, status: number, headers: HttpHeadersGetter, config: RequestConfig) => {
+            this.$scope.shortenedUrl = response.Shortened;
+            this.$scope.waiting = false;
+        }
+
+        onError: HttpPromiseCallback<any> = (response: any, status: number, headers: HttpHeadersGetter, config: RequestConfig) => {
+            console.log("Error: ", response);
+            var message = "Unknown Error";
+            if (status === HttpStatusCodes.Unauthorized) {
+                this.$scope.unauthorized = true;
+                message = "Unauthorized. You must log in to create a short URL.";
+            }
+            if (response.hasOwnProperty("ResponseStatus")) {
+                var responseStatus = <ResponseStatus>response.ResponseStatus;
+                message = responseStatus.Message;
+            }
+            this.$scope.errorMessage = message;
+            this.$scope.waiting = false;
+        };
     }
 
     angular.module("urlShortenerApp", [])
